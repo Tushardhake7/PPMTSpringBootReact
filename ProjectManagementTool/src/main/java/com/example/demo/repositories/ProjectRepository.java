4fd687dd667daa1c5c3054c42cfd17b62c5c394e
Tushar <tushar.dhake@yash.com>
2019-12-17 02:47:02 -0800

    Added project Repository and Project Domain code

---
+++ ProjectManagementTool/src/main/java/com/example/demo/repositories/ProjectRepository.java
@@ -0,0 +1,16 @@
+package com.example.demo.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.domain.Project;
+
+@Repository
+public interface ProjectRepository extends JpaRepository<Project, Long> {
+	
+	@Override
+	List<Project> findAllById(Iterable<Long> iterable);
+
+}
