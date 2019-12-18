98eff7c3ff79ad8841e2ce325f2c018cc519f56b
Tushar <tushar.dhake@yash.com>
2019-12-18 08:40:52 -0800

    Added custom exception to handle database level exception

--- ProjectManagementTool/src/main/java/com/example/demo/services/ProjectService.java
+++ ProjectManagementTool/src/main/java/com/example/demo/services/ProjectService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.domain.Project;
+import com.example.demo.exception.ProjectIdException;
 import com.example.demo.repositories.ProjectRepository;
 
 @Service
@@ -12,10 +13,14 @@ public class ProjectService {
 	@Autowired
 	private ProjectRepository projectRepository;
 	
+	//CustomResponseEntityExceptionHandler
 	public Project saveOrUpdateProject(Project project) {
-		return projectRepository.save(project);
+		try {
+			project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+			return projectRepository.save(project);
+		} catch (Exception e) {
+			throw new ProjectIdException("Project ID '" + project.getProjectIdentifier().toUpperCase() + "'already exists");
+		}	
 	}
-	
-	
 	
 }
