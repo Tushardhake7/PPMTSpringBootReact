08ae6729276a5f73f33b3670508fb708d39930ec
Tushar <tushar.dhake@yash.com>
2019-12-17 23:37:56 -0800

    Mysql connected and save data using save() method

--- ProjectManagementTool/src/main/java/com/example/demo/repositories/ProjectRepository.java
+++ ProjectManagementTool/src/main/java/com/example/demo/repositories/ProjectRepository.java
@@ -1,6 +1,7 @@
 package com.example.demo.repositories;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -10,7 +11,4 @@ import com.example.demo.domain.Project;
 @Repository
 public interface ProjectRepository extends JpaRepository<Project, Long> {
 	
-	@Override
-	List<Project> findAllById(Iterable<Long> iterable);
-
 }
